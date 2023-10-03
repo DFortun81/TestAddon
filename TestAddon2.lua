5736bc4ca47ad8cb8c234c7a4bcd4b12756f5d24
Crieve <dylanfortune@live.com>
2023-10-02 19:49:13 -0700

    Added Default Variables and how to use them as well as load them from the global namespace! Created a default frame that will intercept addon events and provide a simple way of demonstrating how to use SavedVariables. And then demonstrated how to use the ADDON_LOADED event!

--- TestAddon2.lua
+++ TestAddon2.lua
@@ -7,4 +7,76 @@
 local appName, app = ...;
 local L = app.L;
 
+-- Default Variables
+local TestAddonData = _G["TestAddonData"];
+if not TestAddonData then
+	TestAddonData = {
+		relativePoint = "BOTTOMRIGHT",
+		point = "BOTTOMRIGHT",
+		x = 385,
+		y = 500,
+	};
+end
+
 print(L.HELLO_WORLD);
+
+-- Create a Frame and attach an event to it.
+local frame = CreateFrame("FRAME", nil, UIParent, BackdropTemplateMixin and "BackdropTemplate");
+app.frame = frame;
+frame:SetPoint(TestAddonData.relativePoint, TestAddonData.x, TestAddonData.y);
+frame:SetSize(640, 640);
+frame:SetBackdrop({
+	bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+	edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+	tile = true, tileSize = 16, edgeSize = 16,
+	insets = { left = 4, right = 4, top = 4, bottom = 4 }
+});
+frame:SetBackdropBorderColor(1, 1, 1, 1);
+frame:SetBackdropColor(0, 0, 0, 1);
+frame:EnableMouse(true);
+frame:SetMovable(true);
+frame:RegisterForDrag("LeftButton", "RightButton");
+frame:SetScript("OnMouseDown", function(self, button)
+	if button == "LeftButton" then
+		self:StartMoving()
+	end
+end)
+frame:SetScript("OnMouseUp", function(self)
+	self:StopMovingOrSizing();
+	local point, relativeTo, relativePoint, xOfs, yOfs = self:GetPoint()
+	print("WHERE AM I", point, relativePoint, xOfs, yOfs);
+	TestAddonData.relativePoint = relativePoint;
+	TestAddonData.point = point;
+	TestAddonData.x = xOfs;
+	TestAddonData.y = yOfs;
+end)
+
+-- Register some events
+local events = {};
+frame:SetScript("OnEvent", function(self, e, ...) (events[e] or print)(e, ...); end);
+
+
+events.ADDON_LOADED = function(e, addonName)
+	if addonName == appName then
+		print(e, addonName);
+		frame:UnregisterEvent("ADDON_LOADED");
+		
+		local TestAddDataGlobal = _G["TestAddonData"];
+		if TestAddDataGlobal then
+			local changed = false;
+			for key,value in pairs(TestAddDataGlobal) do
+				if TestAddonData[key] ~= value then
+					TestAddonData[key] = value;
+					changed = true;
+				end
+			end
+			_G["TestAddonData"] = TestAddonData;
+			if changed then
+				print("WHERE AM I", TestAddonData.x, TestAddonData.y);
+				frame:ClearAllPoints();
+				frame:SetPoint(TestAddonData.point, TestAddonData.x, TestAddonData.y, TestAddonData.relativePoint);
+			end
+		end
+	end
+end
+frame:RegisterEvent("ADDON_LOADED");
